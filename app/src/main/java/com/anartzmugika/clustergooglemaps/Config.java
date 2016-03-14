87ee8c8e31c8f03c6c33702bb8c4b6422ce7640c
Anartz Mugika <mugan86@gmail.com>
2016-03-14 10:30:06 +0100

    Resize download image without OutOfMemoryError

--- app/src/main/java/com/anartzmugika/clustergooglemaps/Config.java
+++ app/src/main/java/com/anartzmugika/clustergooglemaps/Config.java
@@ -3,6 +3,7 @@ package com.anartzmugika.clustergooglemaps;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.StrictMode;
+import android.util.Log;
 
 import com.anartzmugika.clustergooglemaps.model.Person;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +14,9 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 /***************************************************************
@@ -87,6 +91,107 @@ public class Config {
             e.printStackTrace();
             return null;
         }
+    }
+
+    // 'InputStream' bidez sortu 'Bitmap' bueltatzeko 'doInBackground' metodora
+    public static Bitmap downloadImage(String url) {
+
+        //Fresh Garbage Collector
+        System.gc();
+
+        //Default sample size
+        int sample_size = 1;
+
+        Bitmap bitmap = null;
+        InputStream stream = null;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inScaled=true;
+        bmOptions.inDensity= 160;
+        bmOptions.inPreferredConfig = Bitmap.Config.RGB_565;
+
+
+        boolean imageSuccessFullyLoad = true;
+        boolean image_width_small_than_screen_width = false;
+        do
+        {
+            bmOptions.inSampleSize = sample_size; //Default sample_size = 1
+
+            //url = ConstantValues.URL_LOCALHOST + url.substring(1);
+            System.out.println("URL Image: "+ url);
+
+            try {
+                stream = getHttpConnection(url);
+
+                bitmap = BitmapFactory.
+                        decodeStream(stream, null, bmOptions);
+                stream.close();
+                System.out.println("Downloaded image");
+                //stream.close();
+
+                //Restart
+                imageSuccessFullyLoad = true;
+                image_width_small_than_screen_width = true;
+
+            }
+            catch (OutOfMemoryError oom)
+            {
+                System.out.println ("Out Of Memory Error..");
+                sample_size++;
+
+                System.out.println("Sample size: "+ sample_size);
+                imageSuccessFullyLoad = false;
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+            catch (Exception eu)
+            {
+                System.out.println ("Exception");
+            }
+            System.out.println("Irudia ondo kargatua? "
+                    +imageSuccessFullyLoad
+                    + "\nTamaina egokitua?"+ image_width_small_than_screen_width+"\n"+
+                    "SampleSize: "+sample_size);
+        }
+        while (!imageSuccessFullyLoad && image_width_small_than_screen_width);
+
+        System.out.println ("Irudiari tamaina zehazten...");
+
+
+        return bitmap;
+    }
+
+    private static InputStream getHttpConnection(String urlString)
+            throws IOException {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        InputStream stream = null;
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+
+        try {
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestMethod("GET");
+            httpConnection.connect();
+
+            if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK)
+            {
+                stream = httpConnection.getInputStream();
+            }
+        }
+        catch (SocketTimeoutException e)
+        {
+            Log.e("SocketTimeoutException", "SocketTimeoutException");
+            e.printStackTrace();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return stream;
     }
 
 
